cb6efc93e3720b96233655ac6d171d4f4ff4d85d
RGB-123 <sales@rgb-123.com>
2014-12-06 17:42:13 -0500

    Update ledscapeSetup.sh

--- ledscapeSetup.sh
+++ ledscapeSetup.sh
@@ -9,9 +9,13 @@ cp am335x-boneblack.dtb /boot/uboot/dtbs/
 echo "Modprobing uio_pruss..."
 modprobe uio_pruss
 
+#*Note - As is this function bricks BBB
+# Manually editing uENV.txt is using 
+# vi /boot/uboot/uEnv.txt is the preferred method
+
 #Replace uEnv.txt file that has HDMI disabled
-echo "Disabling HDMI..."
-cp uEnv.txt /boot/uboot/uEnv.txt
+#echo "Disabling HDMI..."
+#cp uEnv.txt /boot/uboot/uEnv.txt
 
 #Set Static IP on BBB
 echo "Setting Static IP..."
